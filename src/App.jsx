98ad7acad0dde2ebb0dc3b1c01863da399812fd9
tesarlukas <tesarlu2@uhk.cz>
2022-07-20 19:56:43 +0200

    Move Task Tracker logic from App to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,14 @@
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Tasks from './components/Tasks';
 import Home from './pages/Home';
 import About from './pages/About';
 import Error from './pages/Error';
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Tidy up',
-      day: '20/5/2022',
-      reminder: false,
-    },
-    {
-      id: 2,
-      text: 'Go buy groceries',
-      day: '26/9/2021',
-      reminder: false,
-    },
-    {
-      id: 3,
-      text: 'Do some work',
-      day: '5/8/2021',
-      reminder: false,
-    },
-    {
-      id: 4,
-      text: 'Do homework',
-      day: '29/7/2021',
-      reminder: false,
-    },
-  ]);
-  // setTasks([...tasks, {}])
-
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id !== id ? task : { ...task, reminder: !task.reminder }
-      )
-    );
-  };
-
   return (
     <Router>
       <div className='app'>
         <nav>
-          <div className='logo'>
-            TrackerApp |
-          </div>
+          <div className='logo'>TrackerApp |</div>
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
         </nav>
@@ -62,7 +18,6 @@ const App = () => {
           <Route path='/about/:topic' element={<About />} />
           <Route path='*' element={<Error />} />
         </Routes>
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         <footer>This is footer</footer>
       </div>
     </Router>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import Tasks from '../components/Tasks';
+import { useState } from 'react';
 
 const Home = () => {
-  return <div>This is the home page</div>;
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Tidy up',
+      day: '20/5/2022',
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: 'Go buy groceries',
+      day: '26/9/2021',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'Do some work',
+      day: '5/8/2021',
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: 'Do homework',
+      day: '29/7/2021',
+      reminder: false,
+    },
+  ]);
+  // setTasks([...tasks, {}])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id !== id ? task : { ...task, reminder: !task.reminder }
+      )
+    );
+  };
+  return (
+    <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+  );
 };
 
 export default Home;
