1a2586b6609e1f67d33b85e27e9df4985aafd65d
tesarlukas <tesarlu2@uhk.cz>
2022-07-30 21:49:49 +0200

    Add Focus page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
 import Error from './pages/Error';
+import Focus from './pages/Focus';
 
 const App = () => {
   return (
@@ -12,11 +14,13 @@ const App = () => {
           <div className='separator'>|</div>
           <Link to='/'>Home</Link>
           <Link to='/about'>About</Link>
+          <Link to='/focus'>Focus</Link>
         </nav>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about/' element={<About />} />
           <Route path='/about/:topic' element={<About />} />
+          <Route path='/focus/' element={<Focus />} />
           <Route path='*' element={<Error />} />
         </Routes>
         <footer>This is footer</footer>
---
+++ src/Pages/Focus.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Focus = () => {
+  return <div>Focus task</div>;
+};
+
+export default Focus;
