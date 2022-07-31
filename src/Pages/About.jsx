8beeaf564264f4f912191563996ffb740fd38404
tesarlukas <tesarlu2@uhk.cz>
2022-07-31 18:58:08 +0200

    Extend About description

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 import NavButtons from '../components/NavButtons';
 
 const About = () => {
-  const { topic } = useParams();
+  // const { topic } = useParams();
   const containerStyle = {
     border: 'none',
     display: 'flex',
@@ -15,7 +15,9 @@ const About = () => {
     <div className='container' style={containerStyle}>
       <h1 className='text-center'>About This Application</h1>
       <p>
-        This is an application serving as a simple Task Tracker application.
+        This is an application serving as a simple Task Tracker application. Its
+        main purpose is to show practical usage of the React library and its
+        main features in comparison to the Svelte framework.
       </p>
       <NavButtons />
     </div>
